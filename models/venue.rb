c04dd672b3d502ec33a839976cb73fade8a2a8e1
Bertie Croll <bert.croll@gmail.com>
2016-10-22 11:46:11 +0100

    update method added to venue class

--- models/venue.rb
+++ models/venue.rb
@@ -2,7 +2,8 @@ require_relative('../db/sql_runner')
 
 class Venue
 
-  attr_reader(:id, :name, :location, :capacity)
+  attr_reader(:id)
+  attr_accessor(:name, :location, :capacity)
 
   def initialize(options)
     @id = options['id'].to_i
@@ -17,6 +18,13 @@ class Venue
       RETURNING *"
     venue = SqlRunner.run(sql).first
     @id = venue['id'].to_i
+  end
+
+  def update()
+    sql = "UPDATE venues
+      SET name = '#{@name}', location = '#{@location}', capacity = #{@capacity}
+      WHERE id = #{@id}"
+    SqlRunner.run(sql)
   end
 
   def delete()
