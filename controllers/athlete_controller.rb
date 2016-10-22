bf6689e10e09eca9477bfb7923e8cf5404581814
Bertie Croll <bert.croll@gmail.com>
2016-10-22 12:04:14 +0100

    athlete_controller created

---
+++ controllers/athlete_controller.rb
@@ -0,0 +1,22 @@
+require_relative('../models/athlete')
+
+#INDEX
+#get all athletes /athletes
+
+#NEW
+#get new form to add athlete /athletes/new
+
+#CREATE
+#post new athlete onto list /athletes
+
+#SHOW
+#get single athlete by unique ID /athletes/:id
+
+#EDIT
+#get new form to update athlete /athletes/:id/edit
+
+#UPDATE
+#put update athlete details /athletes/:id
+
+#DESTROY
+#delete athlete using unqiue id, redirect to /athletes
