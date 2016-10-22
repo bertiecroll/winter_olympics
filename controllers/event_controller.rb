cbac0557ec9f6fb13872a3d940ffcc3323760c99
Bertie Croll <bert.croll@gmail.com>
2016-10-22 18:08:57 +0100

    event_controller added

---
+++ controllers/event_controller.rb
@@ -0,0 +1,22 @@
+require_relative('../models/event')
+
+#INDEX
+#get all events /events
+
+#NEW
+#get new form to add event /events/new
+
+#CREATE
+#post new event onto list /events
+
+#SHOW
+#get single event by unique ID /events/:id
+
+#EDIT
+#get new form to update event /events/:id/edit
+
+#UPDATE
+#put update event details /events/:id
+
+#DESTROY
+#delete event using unqiue id, redirect to /events
