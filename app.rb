b5870a598552db441de70b01eadeb71cde5d28d5
Bertie Croll <bert.croll@gmail.com>
2016-10-22 16:20:46 +0100

    sport_controller created

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require('sinatra')
 require('sinatra/contrib/all')
 require_relative('./controllers/athlete_controller')
 require_relative('./controllers/nation_controller')
+require_relative('./controllers/sport_controller')
 
 
 
---
+++ controllers/sport_controller.rb
@@ -0,0 +1,22 @@
+require_relative('../models/sport')
+
+#INDEX
+#get all sports /sports
+
+#NEW
+#get new form to add sport /sport/new
+
+#CREATE
+#post new sport onto list /sports
+
+#SHOW
+#get single sport by unique ID /sports/:id
+
+#EDIT
+#get new form to update sport /sports/:id/edit
+
+#UPDATE
+#put update sport details /sport/:id
+
+#DESTROY
+#delete sport using unqiue id, redirect to /sport
